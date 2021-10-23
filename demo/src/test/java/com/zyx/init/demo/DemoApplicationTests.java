770e2d324ffc3eb27ca0957801d54a1ab2d95062
zyx1001 <1871847488@qq.com>
2021-10-23 14:48:35 +0800

    初始化项目

---
+++ demo/src/test/java/com/zyx/init/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zyx.init.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
